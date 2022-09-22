4b7aa3557b9298e974c343857b3c49859bd772e1
BambergT <chubak.vitalii@gmail.com>
2022-09-22 04:44:06 +0200

    Update Odiqo
    Add Burger Menu to mobile version.

--- Odiqo/assets/js/main.js
+++ Odiqo/assets/js/main.js
@@ -13,3 +13,19 @@
 }());
 
 // HEADER BACKGROUND ON FUNCTION END
+
+// BURGER HANDLER START
+
+(function () {
+	const burgetItem = document.querySelector('.burger');
+	const menu = document.querySelector('.header__nav');
+	const menuClouse = document.querySelector('.header__nav__close');
+	burgetItem.addEventListener('click', () => {
+		menu.classList.add('header__nav__active');
+	});
+	menuClouse.addEventListener('click', () => {
+		menu.classList.remove('header__nav__active');
+	});
+}());
+
+// BURGER HANDLER END
