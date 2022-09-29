f197b0d8054eb2493716830889c58e6baf83d4e7
BambergT <chubak.vitalii@gmail.com>
2022-09-29 23:37:57 +0200

    Optimized

--- Halloween/assets/js/main.js
+++ Halloween/assets/js/main.js
@@ -20,7 +20,7 @@
 	const burgetItem = document.querySelector('.burger');
 	const menu = document.querySelector('.header__nav');
 	const menuClouse = document.querySelector('.header__nav__close');
-	const menuLinks = document.querySelectorAll('.header_link')
+	const menuLinks = document.querySelectorAll('.header__link')
 	burgetItem.addEventListener('click', () => {
 		menu.classList.add('header__nav__active');
 	});
@@ -43,7 +43,7 @@
 (function () {
 
     const smoothScroll = function (targetEl, duration) {
-        const headerElHeight =  document.querySelector('.header').clientHeight;
+        const headerElHeight =  document.querySelector('header').clientHeight;
         let target = document.querySelector(targetEl);
         let targetPosition = target.getBoundingClientRect().top - headerElHeight;
         let startPosition = window.pageYOffset;
@@ -80,3 +80,65 @@
 }());
 
 // SMOOTH SCROLLING FUNCTION END
+
+
+
+
+ document.addEventListener('DOMContentLoaded', function () {
+      // конечная дата
+      const deadline = new Date(2022, 9, 31);
+      // id таймера
+      let timerId = null;
+      // склонение числительных
+      function declensionNum(num, words) {
+        return words[(num % 100 > 4 && num % 100 < 20) ? 2 : [2, 0, 1, 1, 1, 2][(num % 10 < 5) ? num % 10 : 5]];
+      }
+      // вычисляем разницу дат и устанавливаем оставшееся времени в качестве содержимого элементов
+      function countdownTimer() {
+        const diff = deadline - new Date();
+        if (diff <= 0) {
+          clearInterval(timerId);
+        }
+        const days = diff > 0 ? Math.floor(diff / 1000 / 60 / 60 / 24) : 0;
+        const hours = diff > 0 ? Math.floor(diff / 1000 / 60 / 60) % 24 : 0;
+        const minutes = diff > 0 ? Math.floor(diff / 1000 / 60) % 60 : 0;
+        // const seconds = diff > 0 ? Math.floor(diff / 1000) % 60 : 0;
+        $days.textContent = days < 10 ? '0' + days : days;
+        $hours.textContent = hours < 10 ? '0' + hours : hours;
+        $minutes.textContent = minutes < 10 ? '0' + minutes : minutes;
+        // $seconds.textContent = seconds < 10 ? '0' + seconds : seconds;
+       $days.dataset.title = declensionNum(days, ['Days', 'Days', 'Days']);
+       $hours.dataset.title = declensionNum(hours, ['Hours', 'Hours', 'Hours']);
+       $minutes.dataset.title = declensionNum(minutes, ['Minu', 'Min', 'Min']);
+        // $seconds.dataset.title = declensionNum(seconds, ['seconds', 'seconds', 'seconds']);
+      }
+      // получаем элементы, содержащие компоненты даты
+      const $days = document.querySelector('.timer__days');
+      const $hours = document.querySelector('.timer__hours');
+      const $minutes = document.querySelector('.timer__minutes');
+      // const $seconds = document.querySelector('.timer__seconds');
+      // вызываем функцию countdownTimer
+      countdownTimer();
+      // вызываем функцию countdownTimer каждую секунду
+      timerId = setInterval(countdownTimer, 1000);
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
