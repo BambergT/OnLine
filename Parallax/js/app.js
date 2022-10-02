25775d748289226064f9dad956357dfd2649bd5c
BambergT <chubak.vitalii@gmail.com>
2022-10-02 23:51:51 +0200

    Parallax [update]
    - add favicon
    - optimized

--- Parallax/js/app.js
+++ Parallax/js/app.js
@@ -1,8 +1,1 @@
-window.addEventListener('scroll', e => {
-	document.documentElement.style.setProperty('--scrollTop', `${this.scrollY}px`) // Update method
-})
-gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
-ScrollSmoother.create({
-	wrapper: '.wrapper',
-	content: '.content'
-})
+window.addEventListener("scroll",e=>{document.documentElement.style.setProperty("--scrollTop",`${this.scrollY}px`)}),gsap.registerPlugin(ScrollTrigger,ScrollSmoother),ScrollSmoother.create({wrapper:".wrapper",content:".content"});
